6dc6f291dce625a1d666fc8bada478059671b714
Veer Bhadra Singh Solanki <60712391+veerbhadrasolanki@users.noreply.github.com>
2022-05-28 21:53:56 +0530

    Update connection.js

--- connection/connection.js
+++ connection/connection.js
@@ -1,6 +1,6 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://veerbhadrasolanki:OcLPmA6ridURiY3W@cluster0.snegw.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://veerbhadrasolanki:pass@cluster0.snegw.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
